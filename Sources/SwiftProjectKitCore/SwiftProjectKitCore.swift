e3ef7678dec2e9c241fdaf2ee2b7aea836df7bfa
Gigi <42325924+g-cqd@users.noreply.github.com>
2026-01-01 04:25:36 +0100

    chore: bump version to 0.0.27

--- Sources/SwiftProjectKitCore/SwiftProjectKitCore.swift
+++ Sources/SwiftProjectKitCore/SwiftProjectKitCore.swift
@@ -4,7 +4,7 @@
 @_exported import Foundation
 
 /// SwiftProjectKit version - keep in sync with VERSION file
-public let swiftProjectKitVersion = "0.0.26"
+public let swiftProjectKitVersion = "0.0.27"
 
 /// Default Swift version for new projects
 public let defaultSwiftVersion = "6.2"
